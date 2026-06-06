cd214d9076b3b0099039e880ff1a4be825265166
ARIJIT KUNDU <arikundu9@gmail.com>
2026-06-06 23:15:43 +0530

    feat: app id changed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "in.arijitk.synapse"
+    namespace = "in.arijitk.synapse_flutter"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -26,7 +26,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "in.arijitk.synapse"
+        applicationId = "in.arijitk.synapse_flutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
